9a739bef13db05d52f1590a0840e84bb6c79dfeb
nina <juan.amado.sicat.cleto@gmail.com>
2025-08-01 21:03:28 +0800

    Le Third

--- main.cpp
+++ main.cpp
@@ -30,9 +30,29 @@ public:
         return Counter(temp);
     }
 
+    Counter operator -- (int) {
+        --count;
+
+        Counter temp;
+        temp.count = count;
+        return temp;
+    }
+
+    Counter operator + (Counter const& o) {
+        Counter temp;
+        temp.count = count + o.count;
+        return temp;
+    }
+
     Counter operator - (Counter const& o) {
         Counter temp;
         temp.count = count - o.count;
+        return temp;
+    }
+
+    Counter operator * (Counter const& o) {
+        Counter temp;
+        temp.count = count * o.count;
         return temp;
     }
 
